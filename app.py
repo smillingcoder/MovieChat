66243dadd4c1e5e5a06ee1f6922087de3516f36c
Sohum Tiwari <sohumtiwari@gmail.com>
2026-02-25 16:19:27 +0530

    Enhance movie search and database update logic
    Added search_movie_by_title function and updated conditions for database refresh and movie search functionality.

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask,render_template,request
 import threading
 from db import database_setup
 from fetch_tmdb import tmdb_process
-from Queries import data_insertion,complete_movie_info,selected_movie,movies_to_display,movie_count,should_refresh,update_table
+from Queries import data_insertion,complete_movie_info,selected_movie,movies_to_display,movie_count,update_table,date_check,search_movie_by_title
 from nlp_model import bot_mood
 from mood_to_genres_map import genres_for_query
 
@@ -16,11 +16,22 @@ startup_started = False
 @app.route("/",methods=["GET","POST"])
 def home():
     global DATA_READY
+
+    if not DATA_READY:
+        return render_template(
+            "index.html",
+            reply="⏳ Database is setting up. Please refresh in a few seconds.",
+            total_movies=0,
+            total_results=0,
+            your_mood="",
+            genres=[]
+        )
     reply=None
     final_movies = []
     mood_genres=[]
     mood_dict = {"label": ""}
     user=request.form.get("user","").strip()
+    user2=request.form.get("search","").strip()
     if request.method=="POST":
         if not DATA_READY:
             reply=f"⏳Database is still setting up. Please try again in a moment.{DATA_READY}"
@@ -31,9 +42,16 @@ def home():
             rows=complete_movie_info()
             selected_rows=selected_movie(rows,mood_genres)
             final_movies=movies_to_display(selected_rows)
+            reply=final_movies
             if not final_movies:
-                reply="enter a valid text"
-            else:reply=final_movies
+                reply="please enter a valid text"
+        elif user2:
+            results = search_movie_by_title(user2)
+            if results:
+                reply = results
+                final_movies = results
+            else:
+                reply = "❌ No movies found!"
         else:
             reply="enter something..."
 
@@ -49,7 +67,7 @@ def initialize():
     DATA_READY=False
     database_setup()
 
-    if should_refresh() or  not movie_count():
+    if date_check() or not movie_count():
         update_table()
         data_insertion(tmdb_process())
     DATA_READY=True
@@ -70,7 +88,9 @@ def start_background_task():
 
 
 if (__name__)==("__main__"):
-    app.run(debug=True,use_reloader=False)
+    app.run(host="0.0.0.0", port=7860, debug=False)
+
+
 
 
 
